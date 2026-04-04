c1ee88856c06c592db297c74f4667927cc9ad0b4
TUSHAR KALYANI <tusharkalyani@TUSHARs-MacBook-Air.local>
2026-04-04 19:08:05 +0530

    fix: add root page redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
   images: {
     remotePatterns: [
       {
@@ -27,20 +20,8 @@ const nextConfig = {
       },
     ],
   },
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-
-  experimental: {
-    serverActions: {
-      allowedOrigins: ["prep-pilot-blush.vercel.app"],
-    },
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
